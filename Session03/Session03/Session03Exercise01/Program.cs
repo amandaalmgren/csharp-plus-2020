c295bdcc7926f8c7b4761ae79dec056efbff28cc
LAPTOP-CRMN6R41\amand <amandaalmgren@hotmail.com>
2020-08-28 09:16:06 +0200

    Session 03 - Exercise 01

---
+++ Session03/Session03/Session03Exercise01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Session03Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+            var input = Console.ReadLine();
+
+            var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in inputArray)
+            {
+                Console.WriteLine("Värdet är " + number);
+
+            }
+
+            
+        }
+    }
+}
